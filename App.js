ec5981aed5e4c3e9d63f3f28ad69da64615d7f90
“React <reactdev@yandex.com>
2023-04-24 20:43:31 -0400

    Setup

---
+++ App.js
@@ -0,0 +1,10 @@
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+import {View} from 'react-native';
+
+function App() {
+  return (
+    <View />
+  )
+}
+
+export default App;
